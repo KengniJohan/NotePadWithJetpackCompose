496bb949a115c2462527760e6a60d9252af0742b
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2023-03-19 23:12:33 +0100

    Création de la classe faisant office d'application

---
+++ app/src/main/java/android/training/notepadwithjetpackcompose/NoteApp.kt
@@ -0,0 +1,8 @@
+package android.training.notepadwithjetpackcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp : Application() {
+}
